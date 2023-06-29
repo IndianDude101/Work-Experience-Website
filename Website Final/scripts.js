20a0b1d9439a43f0db1634f47481cf6fe2b45bbd
Laksh Patel <68946802+IndianDude101@users.noreply.github.com>
2023-06-29 19:15:06 +0100

    Add files via upload

---
+++ Website Final/scripts.js
@@ -0,0 +1,11 @@
+document.getElementById("book").onclick = function(){
+    location.href = "https://www.myvue.com/film/spider-man-across-the-spider-verse"
+}
+
+document.getElementById("cast").onclick = function(){
+    location.href = "cast.html"
+}
+
+document.getElementById("home").onclick = function(){
+    location.href = "index.html"
+}
